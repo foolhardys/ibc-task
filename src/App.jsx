372524b4f33cd333125c59263fc4ccaeb6ce269d
Siddhant <siddhantrsen73938@gmail.com>
2023-08-05 12:42:10 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import Document from './components/Document'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import NavBar from './components/NavBar'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
+
+function App() {
+
+  return (
+    <>
+      <NavBar/>
+      <Home/>
+      <Document/>
+      <Projects/>
+      <Skills/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
